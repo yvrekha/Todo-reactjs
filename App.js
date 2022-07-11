00c02f5130133f7b4605bc4515d988405dfd58f6
yvrekha <100177542+yvrekha@users.noreply.github.com>
2022-07-11 18:07:02 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './components/Card';
+import Badge from './components/Badge';
+
+const App =() => {
+  return(
+    <div>
+      <Badge/>
+      <Card/>
+    </div>
+  )
+}
+
+export default App;
